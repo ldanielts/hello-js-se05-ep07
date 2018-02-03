3d16979ad85e365c2f5da9195c872d2e4a80183c
unknown <Fanor@FDN-WS-023535.acad.dvuadmin.net>
2018-02-03 15:29:11 -0300

    fix

--- se05ep07-client/src/main.js
+++ se05ep07-client/src/main.js
@@ -6,20 +6,16 @@
 // console.log(bar(3))
 
 // alert("hello from browserify")// alert só é válido no browser
-
 require("./main.css")
 
 const Vue = require("vue")
+const VueRouter = require("vue-router")
 const VueMaterial = require("vue-material")
-const VueRouter = require("vue-router");
 
 Vue.use(VueRouter)
-
 Vue.use(VueMaterial)
-
-Vue.component("hello-vue", require("./spa.vue"))
 
 new Vue({
   el: "#mountpoint",
-  render: r => r("hello-vue")
+  render: r => r(require("./spa.vue"))
 })
